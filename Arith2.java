b6e064cfd58d119351807f0bcc11c8e8beb2dfa8
krishnaprasant <kvkrishnaprasant@yahoo.co.in>
2015-01-09 16:01:00 +0530

    Update Arith2.java

--- Arith2.java
+++ Arith2.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
-public class Arith2 {
-	public static void main(String[] args) {
-
-		Scanner sci = new Scanner(System.in);
-		Scanner scl = new Scanner(System.in);
-		Scanner scc = new Scanner(System.in);
+class Arith2
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
 		char ch='\0';
-		int T = sci.nextInt();
+		int T = sc.nextInt();
 		while(T!=0){
 			System.out.println();
-			long a = scl.nextLong();
+			long a = sc.nextLong();
 			while(true){
-				System.out.println(" ");
-				ch = scc.next().charAt(0);
+				ch = sc.next().charAt(0);
 				if(ch == '=')
 					break;
-				System.out.println(" ");
-				int b = sci.nextInt();
+				int b = sc.nextInt();
 				if(ch=='/')
 					a=a/b;
 				else if(ch=='*')
@@ -31,5 +28,3 @@ public class Arith2 {
 		}
 	}
 }
-
-
